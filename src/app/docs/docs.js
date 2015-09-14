8787f189b75295b8955f2a6e24bfaa82f048de58
Robert <rwatt@four51.com>
2015-09-14 16:32:06 -0500

    Basic Docs skeleton initial commit

--- src/app/docs/docs.js
+++ src/app/docs/docs.js
@@ -11,11 +11,21 @@ function DocsConfig( $stateProvider ) {
 			url: '/docs',
 			templateUrl:'docs/templates/docs.tpl.html',
 			controller:'DocsCtrl',
-			controllerAs: 'docs'
+			controllerAs: 'docs',
+			resolve: {
+				Documentation: function(Docs) {
+					return Docs.GetAll();
+				}
+			}
 		})
 }
 
-function DocsController( ) {
+function DocsController( Documentation ) {
 	var vm = this;
-
+	vm.content = Documentation;
+	vm.setMaxLines = function(editor) {
+		editor.setOptions({
+			maxLines:100
+		});
+	}
 }
