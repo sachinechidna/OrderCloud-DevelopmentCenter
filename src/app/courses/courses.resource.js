4a6e603297f97f9f496720c32769103514469607
Robert <rwatt@four51.com>
2015-09-28 14:34:40 -0500

    Image reference in courses list fix

--- src/app/courses/courses.resource.js
+++ src/app/courses/courses.resource.js
@@ -28,7 +28,7 @@ function CoursesService($q, Underscore) {
 			Description: 'Familiarize yourself with the OrderCloud RESTful API and the toolsets available for developing against it.',
 			Difficulty: 'Beginner',
 			Classes: ['api','ordercloud','tools','sdk'],
-			ImgUrl: '/assets/intro.png'
+			ImgUrl: 'assets/intro.png'
 		},
 		{
 			ID: 'basics',
@@ -36,7 +36,7 @@ function CoursesService($q, Underscore) {
 			Description: 'A real-world, interactive introduction to help you get started using OrderCloud',
 			Difficulty: 'Beginner',
 			Classes: ['create-buyer', 'create-buyer-user', 'api-access', 'get-me'],
-			ImgUrl: '/assets/basics.png'
+			ImgUrl: 'assets/basics.png'
 		},
 		{
 			ID: 'user-prod-access',
@@ -44,7 +44,7 @@ function CoursesService($q, Underscore) {
 			Description: 'Tackle the challenge of making a product accessible to a buyer-user',
 			Difficulty: 'Intermediate',
 			Classes: [ 'prod-crud', 'price-sched-crud', 'user-crud', 'prod-assignments', 'category-crud', 'category-prod-assignment', 'category-assignment', 'get-products-as-user'],
-			ImgUrl: '/assets/productAsBuyer.png'
+			ImgUrl: 'assets/productAsBuyer.png'
 		}
 	];
 
