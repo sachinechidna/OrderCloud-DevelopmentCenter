fed153fd020bb0e3f99e2ac9b16a2c4900c89350
Robert <rwatt@four51.com>
2015-09-29 13:59:23 -0500

    Gulp Config change

--- gulpConfig.js
+++ gulpConfig.js
@@ -64,15 +64,15 @@ module.exports = {
 		import_less: ['vendor/font-awesome/less/font-awesome.less'],
 		css: [],
 		js: [
-			'vendor/ace-builds/src-min-noconflict/ace.js',
-			'vendor/ace-builds/src-min-noconflict/mode-javascript.js',
-			'vendor/ace-builds/src-min-noconflict/mode-markdown.js',
-			'vendor/ace-builds/src-min-noconflict/mode-json.js',
-			'vendor/ace-builds/src-min-noconflict/theme-twilight.js',
-			'vendor/ace-builds/src-min-noconflict/theme-idle_fingers.js',
+			'vendor/ace-builds/src/ace.js',
+			'vendor/ace-builds/src/mode-javascript.js',
+			'vendor/ace-builds/src/mode-markdown.js',
+			'vendor/ace-builds/src/mode-json.js',
+			'vendor/ace-builds/src/theme-twilight.js',
+			'vendor/ace-builds/src/theme-idle_fingers.js',
 			'vendor/ordercloud-angular-sdk/dist/ordercloud-angular-sdk.js',
-			'vendor/ace-builds/src-min-noconflict/worker-json.js',
-			'vendor/ace-builds/src-min-noconflict/worker-javascript.js'
+			'vendor/ace-builds/src/worker-json.js',
+			'vendor/ace-builds/src/worker-javascript.js'
 		],
 		exclude_js: [
 			'!vendor/ordercloud-angular-sdk/dist/ordercloud-angular-sdk.min.js'
